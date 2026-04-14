b5c2023e240edc86d8427b6f5a90f0050043b42a
Fatih <47229809+fatihmrcn97@users.noreply.github.com>
2026-04-14 12:43:44 +0300

    Configure GitHub Pages deployment with GitHub Actions

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  output: "export",
+  basePath: "/VaganzaWebPage",
   images: {
+    unoptimized: true,
     remotePatterns: [
       {
         protocol: "https",
